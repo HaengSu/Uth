8ae4f7f8b78df240bcb3a6d78a2a14a1c3eee46a
HaengSu <onse384@gmail.com>
2024-12-17 09:35:31 +0900

    feature : SignUp page_3 UI 구현 중
    step_3_page.dart 페이지 ui 구현 셋팅 중

--- lib/screens/sign/step_3_page.dart
+++ lib/screens/sign/step_3_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:uth/common/theme/colors.dart';
 import 'package:uth/screens/data/UserProfile.dart';
-import 'package:uth/screens/main.dart';
 
 /**
- * 두번째 페이지
+ * 세번째 페이지
  */
 class InputUserProfilePage extends StatefulWidget {
   final PageController pageController;
@@ -25,8 +23,7 @@ class _InputUserProfilePage extends State<InputUserProfilePage> {
   final TextEditingController textInputNickName = TextEditingController();
   final ValueNotifier<bool?> isNickNameExist = ValueNotifier<bool?>(null);
 
-  late UserProfile userProfile;   // 유저의 회원가입시 데이터를 담을 변수
-
+  late UserProfile userProfile; // 유저의 회원가입시 데이터를 담을 변수
 
   @override
   void initState() {
@@ -58,171 +55,106 @@ class _InputUserProfilePage extends State<InputUserProfilePage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                /**
-                 * 상단 비밀번호 입력창
-                 */
                 TextFormField(
-                  controller: textInputFirstPW,
-                  obscureText: _isFirstObscured,
-                  // 비밀번호 가리는 옵션
-                  keyboardType: TextInputType.number,
-                  // 키보드 형식 지정
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  onChanged: (text) {
-                    setState(() {});
-                  },
-
                   decoration: InputDecoration(
-                      hintText: "비밀번호 입력",
-                      hintStyle: const TextStyle(color: grayColor_BD),
-                      counterText: '',
-                      enabledBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: grayColor_BD, // 기본(선택되지 않은) 상태의 색상
-                        ),
+                    hintText: "이름",
+                    hintStyle: const TextStyle(color: grayColor_BD),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: grayColor_BD, // 기본(선택되지 않은) 상태의 색상
                       ),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: mainColor, // 원하는 색상으로 변경
-                        ),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: mainColor, // 원하는 색상으로 변경
                       ),
-                      suffixIcon: textInputFirstPW.text.isNotEmpty
-                          ? Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        //text visiable 조절
-                                        _isFirstObscured = !_isFirstObscured;
-                                      });
-                                    },
-                                    icon: const Icon(
-                                      Icons.visibility_off,
-                                      color: grayColor_BD,
-                                      size: 24,
-                                    )),
-                                IconButton(
-                                    onPressed: () {
-                                      textInputFirstPW.clear();
-                                      setState(() {});
-                                    },
-                                    icon: const Icon(
-                                      Icons.cancel_sharp,
-                                      color: grayColor_BD,
-                                      size: 24,
-                                    ))
-                              ],
-                            )
-                          : null,
-                      focusedErrorBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                        color: _isPWMatch.value == true
-                            ? mainColor
-                            : errorColor, // 에러 상태에서 포커스가 있는 경우
-                        width: 2,
-                      ))),
+                    ),
+                  ),
                 ),
                 const SizedBox(width: 7),
                 /**
-                 * 하단 비밀번호 입력창
+                 *  닉네임 입력란
                  */
-                TextFormField(
-                  controller: textInputSecondPW,
-                  obscureText: _isSecondObscured,
-                  // 비밀번호 가리는 옵션
-                  keyboardType: TextInputType.number,
-                  // 키보드 형식 지정
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  onChanged: (inputText) {
-                    if (inputText.length == textInputFirstPW.text.length) {
-                      // 입력 text 길이가 같을 경우 유효성 검사 진행
-                      if (inputText == textInputFirstPW.text) {
-                        _isPWMatch.value = true;
-                      } else {
-                        _isPWMatch.value = false;
-                      }
-                    } else {
-                      if (inputText.length == 0) {
-                        _isPWMatch.value = null;
-                      } else {
-                        _isPWMatch.value = false;
-                      }
-                    }
-                    debugPrint(
-                        "##INFO >> inputText.length= ${inputText.length}");
+                ValueListenableBuilder(
+                    valueListenable: isNickNameExist,
+                    builder: (context, isDuplicated, child) {
+                      return Container(
+                        margin:
+                            const EdgeInsets.only(top: 27, left: 20, right: 20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              // TextField가 가능한 공간을 모두 차지하도록 설정
+                              child: TextFormField(
+                                controller: textInputNickName,
+                                decoration: InputDecoration(
+                                    hintText: "닉네임",
+                                    hintStyle:
+                                        const TextStyle(color: grayColor_BD),
+                                    counterText: '',
+                                    enabledBorder: const UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color:
+                                            grayColor_BD, // 기본(선택되지 않은) 상태의 색상
+                                      ),
+                                    ),
+                                    focusedBorder: const UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: mainColor, // 원하는 색상으로 변경
+                                      ),
+                                    ),
+                                    errorText: checkErrorText(),
+                                    errorStyle: TextStyle(
+                                        color: isDuplicated == true
+                                            ? mainColor
+                                            : errorColor,
+                                        fontSize: 11),
+                                    errorMaxLines: 1,
+                                    errorBorder: const UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: errorColor, width: 2)),
+                                    focusedErrorBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                      color: isDuplicated == true
+                                          ? mainColor
+                                          : errorColor,
+                                      // 에러 상태에서 포커스가 있는 경우
+                                      width: 2,
+                                    ))),
+                              ),
+                            ),
+                            const SizedBox(width: 7), // TextField와 버튼 사이에 여백 추가
 
-                    setState(() {});
-                  },
-
-                  decoration: InputDecoration(
-                      hintText: "비밀번호 확인",
-                      hintStyle: const TextStyle(color: grayColor_BD),
-                      counterText: '',
-                      enabledBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: grayColor_BD, // 기본(선택되지 않은) 상태의 색상
+                            OutlinedButton(
+                                onPressed: () {},
+                                style: OutlinedButton.styleFrom(
+                                  backgroundColor:
+                                      textInputNickName.text.isNotEmpty == true
+                                          ? mainColor
+                                          : Colors.white,
+                                  foregroundColor:
+                                      textInputNickName.text.isNotEmpty == true
+                                          ? Colors.white
+                                          : grayColor_98,
+                                  side: BorderSide(
+                                      color:
+                                          textInputNickName.text.isNotEmpty ==
+                                                  true
+                                              ? mainColor
+                                              : grayColor_E0),
+                                ),
+                                child: Text("중복확인")),
+                          ],
                         ),
-                      ),
-                      // focusedBorder: const UnderlineInputBorder(
-                      //   borderSide: BorderSide(
-                      //     color: mainColor, // 원하는 색상으로 변경
-                      //   ),
-                      // ),
-                      suffixIcon: textInputSecondPW.text.isNotEmpty
-                          ? Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        // text visiable 조절
-                                        _isSecondObscured = !_isSecondObscured;
-                                      });
-                                    },
-                                    icon: const Icon(
-                                      Icons.visibility_off,
-                                      color: grayColor_BD,
-                                      size: 24,
-                                    )),
-                                IconButton(
-                                    onPressed: () {
-                                      textInputSecondPW.clear();
-                                      _isPWMatch.value = null;
-                                      setState(() {});
-                                    },
-                                    icon: const Icon(
-                                      Icons.cancel_sharp,
-                                      color: grayColor_BD,
-                                      size: 24,
-                                    ))
-                              ],
-                            )
-                          : null,
-                      errorText: checkErrorText(),
-                      errorStyle:
-                          TextStyle(color: checkErrorTextColor(), fontSize: 11),
-                      errorMaxLines: 2,
-                      errorBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                        color: checkErrorTextColor(),
-                      )),
-                      focusedErrorBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                        color: checkBorderColor(),
-                        width: 1,
-                      ))),
-                ),
+                      );
+                    }),
               ],
             ),
           ),
           ValueListenableBuilder(
-              valueListenable: _isPWMatch,
-              builder: (context, isMatch, child) {
+              valueListenable: isNickNameExist,
+              builder: (context, isExist, child) {
                 return Padding(
                   padding: const EdgeInsets.only(top: 32, left: 20, right: 20),
                   child: SizedBox(
@@ -243,9 +175,9 @@ class _InputUserProfilePage extends State<InputUserProfilePage> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(12.0)),
                         backgroundColor:
-                            isMatch == true ? mainColor : grayColor_E0,
+                            isExist == true ? grayColor_E0 : mainColor,
                         foregroundColor:
-                            isMatch == true ? Colors.white : grayColor_9E,
+                            isExist == true ? grayColor_9E : Colors.white,
                       ),
                     ),
                   ),
@@ -257,19 +189,18 @@ class _InputUserProfilePage extends State<InputUserProfilePage> {
   }
 
   String? checkErrorText() {
-    debugPrint("##INFO >> _isPWMatch.value = ${_isPWMatch.value}");
-    return (_isPWMatch.value == true || _isPWMatch.value == null)
-        ? "* 대소문자 구별 X, 특수문자, 숫자 합산 8자리 이상"
-        : "* 대소문자 구별 X, 특수문자, 숫자 합산 8자리 이상\n* 비밀번호가 일치하지 않습니다";
+    return (isNickNameExist.value == true || isNickNameExist.value == null)
+        ? "* 중복된 닉네임입니다"
+        : "* 사용가능한 닉네임입니다";
   }
 
   Color checkErrorTextColor() {
     Color resColor = grayColor_9E;
 
-    if (_isPWMatch.value == true) {
-      resColor = mainColor;
-    } else if (_isPWMatch.value == false) {
+    if (isNickNameExist.value == true) {
       resColor = errorColor;
+    } else if (isNickNameExist.value == false) {
+      resColor = mainColor;
     }
 
     return resColor;
@@ -278,10 +209,10 @@ class _InputUserProfilePage extends State<InputUserProfilePage> {
   Color checkBorderColor() {
     Color resColor = mainColor;
 
-    if (_isPWMatch.value == true) {
-      resColor = grayColor_9E;
-    } else if (_isPWMatch.value == false) {
+    if (isNickNameExist.value == true) {
       resColor = errorColor;
+    } else if (isNickNameExist.value == false) {
+      resColor = grayColor_9E;
     }
 
     return resColor;
