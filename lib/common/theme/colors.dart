b552b75d3c60b34b7c7883db3a065daf80d4b824
HaengSu <onse384@gmail.com>
2025-02-13 23:28:34 +0900

    feature : Main 페이지 ui 구현완료
    변경된 main ui로 수정 중

--- lib/common/theme/colors.dart
+++ lib/common/theme/colors.dart
@@ -12,6 +12,7 @@ const grayColor_9E = Color(0xFF9E9E9E);
 const grayColor_E0 = Color(0xFFE0E0E0);
 const grayColor_98 = Color(0xFF989BA2);
 const grayColor_BD = Color(0xFFBDBDBD);
+const grayColor_37 = Color(0x9C37383C);
 
 
 const grayColor_trans_16 = Color(0x4070737C);
--- lib/screens/main/MainPage.dart
+++ lib/screens/main/MainPage.dart
@@ -21,7 +21,7 @@ class _MainPage extends State<MainPage> {
         children: [
           Container(
             padding: EdgeInsets.only(top: 58, left: 20, right: 15, bottom: 24),
-            color: mainColor,
+            color: Colors.white,
             child: Column(
               children: [
                 Row(
@@ -34,12 +34,12 @@ class _MainPage extends State<MainPage> {
                   ],
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 41, bottom: 29),
+                  margin: EdgeInsets.only(top: 30, bottom: 21),
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
                       "버디매칭을 이용하여\n함께 할 버디를 찾아보세요 👀",
-                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      style: TextStyle(color: Colors.black, fontSize: 22),
                     ),
                   ),
                 ),
@@ -47,7 +47,7 @@ class _MainPage extends State<MainPage> {
                   style: TextButton.styleFrom(
                       padding: EdgeInsets.only(
                           top: 22, bottom: 22, right: 18, left: 18),
-                      backgroundColor: blueColor_FF,
+                      backgroundColor: Colors.black,
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(12.0))),
                   child: Row(
@@ -57,13 +57,13 @@ class _MainPage extends State<MainPage> {
                           alignment: Alignment.center, // 텍스트를 가운데에 배치
                           child: Text(
                             "버디 매칭 하러가기",
-                            style: TextStyle(color: Colors.white, fontSize: 16),
+                            style: TextStyle(color: mainColor, fontSize: 16),
                           ),
                         ),
                       ),
                       Icon(
                         Icons.arrow_forward_ios_rounded,
-                        color: Colors.white,
+                        color: grayColor_37,
                       ),
                     ],
                   ),
@@ -74,47 +74,61 @@ class _MainPage extends State<MainPage> {
           ),
           Expanded(
               child: Container(
-            color: whiteColor_F9,
-            child: Column(
-              children: [
-                Row(
+                color: Colors.white,
+                child: Column(
                   children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 20, top: 23),
-                      child: Text("최신 매칭",
-                          style: TextStyle(fontSize: 20, color: Colors.black)),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 20, top: 23),
+                          child: Text("최신 매칭",
+                              style: TextStyle(fontSize: 20, color: Colors
+                                  .black)),
+                        ),
+                        Spacer(),
+                        Container(
+                          margin: EdgeInsets.only(right: 20, top: 34),
+                          child: Text("더보기>",
+                              style: TextStyle(
+                                  fontSize: 12, color: grayColor_BD)),
+                        ),
+                      ],
                     ),
-                    Spacer(),
-                    Container(
-                      margin: EdgeInsets.only(right: 20, top: 34),
-                      child: Text("더보기>",
-                          style: TextStyle(fontSize: 12, color: grayColor_BD)),
-                    ),
+                    Expanded(
+                        child: Container(
+                          margin:
+                          EdgeInsets.only(
+                              left: 20, right: 20, top: 13, bottom: 13),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: blackColor_trans_7,
+                                  offset: Offset(0.0, 1.0), //(x,y)
+                                  blurRadius: 9.0,
+                                )
+                              ],
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(16.0))),
+                          child: listWidget(),
+                        ))
                   ],
                 ),
-                Expanded(
-                    child: Container(
-                  margin:
-                      EdgeInsets.only(left: 20, right: 20, top: 13, bottom: 13),
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: blackColor_trans_7,
-                          offset: Offset(0.0, 1.0), //(x,y)
-                          blurRadius: 9.0,
-                        )
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                  child: ,
-                ))
-              ],
-            ),
-          ))
+              ))
         ],
       ),
     );
   }
+}
+
+Widget listWidget() {
+  return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        return matchingWidget();
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemCount: 15
+  );
 }
 
 Widget matchingWidget() {
